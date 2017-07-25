dc4b23cd59560aeb18a05b420b80da47c821f20b
yovankagonzalez <30446119+yovankagonzalez@users.noreply.github.com>
2017-07-25 19:52:28 +0000

    uploaded loop from pi

---
+++ crazy_script_1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# This is a comment 
+# list files
+ls
+
+# redirect the output of ls to "Mallory"
+ls > Mallory
+
+# Print a blank line
+echo  " " 
+
+# Print "catting Mallory"
+echo "catting Mallory"
+
+# Print the contents of the file "Mallory" 
+cat Mallory
+
+#Print an integer
+echo "Word" 
+
+#Print a word
+echo "Word" 
+
+#Print something 
+echo "location" 
+
+#Print current directory
+pwd
---
+++ test_for_loop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# Repeat this outer loop 3 times (y= 1,2,3)
+for y in 1 2 3 
+do 
+
+#Repeat this inner loop 3 times 
+ for x in "Monday" "Tuesday" "Wednesday"
+
+ do
+    echo $x
+done
+ echo " " 
+done
+
